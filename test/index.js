dc073f051708c5ffe85495f1acfde2ce156f8b4b
Azat Mardan <hi@azat.co>
2016-02-28 08:21:23 -0800

    add changes :small_blue_diamond:

--- test/index.js
+++ test/index.js
@@ -1,36 +1,27 @@
-var test = require('tape'),
-  cp = require('child_process')
-
-var fs = require('fs')
+var expect = require('chai').expect
 var path = require('path')
+var fs = require('fs')
 var database = require(path.join(__dirname, '..', 'config', 'database.json'))
+var main = require(path.join(__dirname, '..', 'main.js'))
 
-test('node main to print domain and port', function (t) {
-  t.plan(2)
-  child = cp.exec('node main',
-  function (error, stdout, stderr) {
-    // console.log('stdout: ' + stdout)
-    // console.log('stderr: ' + stderr)
-    t.equal(stderr, '', 'No errors running main.js')
-    if (error !== null) {
-      console.log('exec error: ' + error)
-    }
-    stdout = stdout.replace('\n','')
+describe('module main.js', function () {
+  it('must export dev domain and port', function(done){
     var domainPort = database.dev.domain + ':' + database.dev.port
-    t.equal(stdout, domainPort, 'Domain and port must be from config/database.json')
+    expect(main).to.equal(domainPort)
+    done()
   })
 })
 
-
-test('main.js to have __dirname and path', function (t) {
-  t.plan(3)
-  try {
-    main = fs.readFileSync(path.join(__dirname, '..', 'main.js'), {encoding: 'utf8'})
-  } catch(e) {
-    t.equal(e, null)
-  }
-  t.ok(main, 'Read the file main.js')
-  t.equal(main.indexOf('__dirname')>-1, true, 'Must use __dirname')
-  t.equal(main.indexOf('.join(')>-1, true, 'Must use path.join()')
-
+describe('module main.js', function () {
+  it('must have __dirname and path', function(done){
+    try {
+      main = fs.readFileSync(path.join(__dirname, '..', 'main.js'), {encoding: 'utf8'})
+    } catch(error) {
+      expect(error).to.be.null
+    }
+    expect(main).to.be.ok
+    expect(main).to.contain('__dirname')
+    expect(main).to.contain('.join(')
+    done()
+  })
 })
